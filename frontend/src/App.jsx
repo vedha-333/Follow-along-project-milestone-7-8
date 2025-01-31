0c5ef0d09c740b7e72a6c2bf599f4d86b11c86ec
vedha-333 <vedantpandilwad@gmail.com>
2025-01-31 15:55:37 +0530

    done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,SignupPage,Home} from "./Routes";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,8 @@
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+import Home from "./pages/Home";
+export {LoginPage,SignupPage,Home};
+
+
+
+
